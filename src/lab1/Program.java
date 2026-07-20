47d60eccf4e8c8789abc08c660837212121c9f82
agray998 <a.gray.4797@gmail.com>
2026-07-20 16:08:51 +0100

    uncomment stuff

--- src/lab1/Program.java
+++ src/lab1/Program.java
@@ -4,30 +4,23 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// String message; // = "Hello World!!";
-		// ...
 		String defaultMessage = args[0];
 		System.out.println(defaultMessage);
 		
-		char c1;
+		char c1, c2;
 		
 		c1 = 'c';
-//		c2 = 'Z';
-//		
-//		System.out.println((int) c1);
-//		System.out.println((int) c2);
+		c2 = 'Z';
 		
-		// Program p = new Program();
+		System.out.println((int) c1);
+		System.out.println((int) c2);
 		
-//		if (Program.isUpper(c1)) {
-//		    System.out.println("Upper Case");
-//		} else if () {
-//			System.out.println("lower case");
-//		} else if () {
-//			
-//		} else {
-//			
-//		}
+		
+		if (Program.isUpper(c1)) {
+		    System.out.println("Upper Case");
+		} else {
+			System.out.println("lower case");
+		}
 		
 		switch (c1) {
 		  case 'a':
