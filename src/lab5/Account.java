59050f56ed194242311bc89dee7fd6c763331bd9
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2026-07-22 11:36:11 +0100

    lab 5 part 1 complete

---
+++ src/lab5/Account.java
@@ -0,0 +1,42 @@
+package lab5;
+
+public class Account {
+	private int id;
+	private String owner;
+	private double balance;
+	
+	public Account(int id, String owner, double balance) {
+		this.id = id;
+		this.owner = owner;
+		this.balance = balance;
+	}
+	
+	public Account(int id, String owner) {
+		this(id, owner, 0);
+	}
+	
+	public void Deposit(double amount) {
+		if (amount < 0) {
+			System.out.printf("Deposit amount %g is invalid: less than zero\n", amount);
+		} else {
+			this.balance += amount;
+		}
+	}
+	
+	public void Withdraw(double amount) {
+		if (amount > this.balance) {
+			System.out.println("Insufficient funds");
+		} else {
+			this.balance -= amount;
+		}
+	}
+	
+	public String getDetails() {
+		return String.format("Account # %d: Owner %s, balance %g", this.id, this.owner, this.balance);
+	}
+	
+	public String getOwner() {
+		return this.owner;
+	}
+
+}
